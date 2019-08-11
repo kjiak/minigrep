f5aa08d38b4cf14da16107fd1622dbcb61a08de0
kjiakai <kjiakai@gmail.com>
2019-08-12 01:57:05 +0800

    add iterator adaptors

--- src/lib.rs
+++ src/lib.rs
@@ -52,7 +52,6 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-  
      contents.lines()
         .filter(|line| line.to_lowercase().contains(&query.to_lowercase()))
         .collect()
